eff766818c6997edd6a984be986d6e7bf22623a1
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2021-09-27 13:47:09 -0500

    implemented virus animation

--- chasm.js
+++ chasm.js
@@ -12,18 +12,15 @@ var scoreText;
 demo.chasm.prototype = {
 
     preload: function(){    
-        game.load.image('chasm', 'assets/chasm.png');
         game.load.image('ground', 'assets/platform.png');
-        game.load.image('ledge', 'assets/ledge.png');
-        game.load.image('apple', 'assets/apple.png');
-        game.load.spritesheet('bird', 'assets/bird.png', 32, 32);
+        game.load.spritesheet('bird', 'assets/virus.png', 32, 32);
     },
     create: function(){
         //  We're going to be using physics, so enable the Arcade Physics system
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
         //  A simple background for our game
-        game.add.sprite(0, 0, 'chasm');
+        // game.add.sprite(0, 0, 'chasm');
 
         //  The platforms group contains the ground and the 2 ledges we can jump on
         platforms = game.add.group();
@@ -51,7 +48,6 @@ demo.chasm.prototype = {
         game.physics.arcade.enable(player);
 
         //  Player physics properties. Give the little guy a slight bounce.
-        player.body.bounce.y = 0.2;
         player.body.gravity.y = 300;
         player.body.collideWorldBounds = true;
 
@@ -59,40 +55,7 @@ demo.chasm.prototype = {
         player.animations.add('left', [0, 1], 4, true);
         player.animations.add('right', [2,3], 4, true);
 
-   
-        apples = game.add.group();
 
-        apples.enableBody = true;
-
-        //  Here we'll create 12 of them evenly spaced apart
-        for (var i = 0; i < 10; i++)
-        {
-            var apple = apples.create(i * 70, game.world.height - 200, 'apple');
-
-            //  Let gravity do its thing
-            apple.body.gravity.y = 200;
-
-            //  This just gives each star a slightly random bounce value
-            apple.body.bounce.y = 0.7 + Math.random() * 0.2;
-
-            applecount += 1;
-        }
-
-        for (var i = 0; i < 10; i++)
-        {
-            //  Create a star inside of the 'stars' group
-            var apple = apples.create(900 + i * 70, game.world.height - 200, 'apple');
-
-            //  Let gravity do its thing
-            apple.body.gravity.y = 200;
-
-            //  This just gives each star a slightly random bounce value
-            apple.body.bounce.y = 0.7 + Math.random() * 0.2;
-
-            applecount += 1;
-        }
-        //  The score
-        scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
         //  Our controls.
         cursors = game.input.keyboard.createCursorKeys();
