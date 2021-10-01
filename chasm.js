e356feec7ceaa0110ba0d30118f80db804526748
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2021-10-01 15:48:46 -0500

    added automated virus animation and deleted all unnceessary code

--- chasm.js
+++ chasm.js
@@ -1,5 +1,5 @@
 
-var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, adam, speed = 6;
+var demo = {}, centerX = 1400 / 2, centerY = 900 / 2, adam, speed = 6;
 demo.chasm = function(){};
 var player;
 var platforms;
@@ -8,52 +8,22 @@ var cursors;
 var apples;
 var applecount = 0;
 var scoreText;
+var anim;
 
 demo.chasm.prototype = {
 
     preload: function(){    
         game.load.image('ground', 'assets/platform.png');
-        game.load.spritesheet('bird', 'assets/virus.png', 32, 32);
+        game.load.spritesheet('virus', 'assets/virus.png', 32, 37);
     },
     create: function(){
-        //  We're going to be using physics, so enable the Arcade Physics system
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-
-        //  A simple background for our game
-        // game.add.sprite(0, 0, 'chasm');
-
-        //  The platforms group contains the ground and the 2 ledges we can jump on
-        platforms = game.add.group();
-
-        //  We will enable physics for any object that is created in this group
-        platforms.enableBody = true;
-
-        // Here we create the ground.
-        var ground = platforms.create(-150, game.world.height - 200, 'ground');
-        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-        ground.scale.setTo(2, 2);
-        
-        var ground2 = platforms.create(860, game.world.height - 200, 'ground');
-        ground2.scale.setTo(2, 2);
-        //  This stops it from falling away when you jump on it
-        ground.body.immovable = true;
-        ground2.body.immovable = true;
 
 
+        virus = game.add.sprite(64, game.world.height - 250, 'virus');
+        virus.scale.set(3);
 
-        // The player and its settings
-        player = game.add.sprite(64, game.world.height - 250, 'bird');
-
-        //  We need to enable physics on the player
-        game.physics.arcade.enable(player);
-
-        //  Player physics properties. Give the little guy a slight bounce.
-        player.body.gravity.y = 300;
-        player.body.collideWorldBounds = true;
-
-        //  Our two animations, walking left and right.
-        player.animations.add('left', [0, 1], 4, true);
-        player.animations.add('right', [2,3], 4, true);
+        anim = virus.animations.add('run');
+        anim.play(10,true);
 
 
 
@@ -62,46 +32,11 @@ demo.chasm.prototype = {
    
     },
     update: function(){
-        var hitPlatform = game.physics.arcade.collide(player, platforms);
-        game.physics.arcade.collide(apples, platforms);
+        virus.x -= 2;
 
-        game.physics.arcade.overlap(player, apples, collectApple, null, this);
-
-        //  Reset the players velocity (movement)
-        player.body.velocity.x = 0;
-
-        if (cursors.left.isDown)
+        if (virus.x < -virus.width)
         {
-            //  Move to the left
-            player.body.velocity.x = -150;
-
-            player.animations.play('left');
-        }
-        else if (cursors.right.isDown)
-        {
-            //  Move to the right
-            player.body.velocity.x = 150;
-
-            player.animations.play('right');
-        }
-        else
-        {
-            //  Stand still
-            player.animations.stop();
-
-            player.frame = 4;
-        }
-        
-        //  Allow the player to jump if they are touching the ground.
-        if (cursors.up.isDown && player.body.touching.down && hitPlatform)
-        {
-            player.body.velocity.y = -350;
-        }    
-
-
-        if (gameState.score == 10*(applecount-1))
-        {
-            game.state.start('forest');
+            virus.x = game.world.width;
         }
 
     }
@@ -109,14 +44,5 @@ demo.chasm.prototype = {
 
 
 
-function collectApple (player, apple) {
-    
-    // Removes the star from the screen
-    apple.kill();
 
-    //  Add and update the score
-    gameState.score += 10;
-    scoreText.text = 'Score: ' + gameState.score;
-
-}
 
