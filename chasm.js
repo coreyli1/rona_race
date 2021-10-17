ae492aafca452b21eb5123d22e56e9c87b3b4415
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2021-10-17 16:11:20 -0500

    implemented error and backspace + new background

--- chasm.js
+++ chasm.js
@@ -12,13 +12,13 @@ var anim;
 var loc = 0;
 var speed = 10;
 var bmd;
-var word = "a quick brown fox moves";
+var word = "The quick brown fox jumps over the lazy dog. Learning how to type is important.";
 var correct = [];
 var previous = '';
 demo.chasm.prototype = {
 
     preload: function(){ 
-        game.load.image('track', 'assets/racetrack.png');
+        game.load.image('track', 'assets/background.png');
         game.load.image('textbox', 'assets/textbox.jpg');
         game.load.spritesheet('virus', 'assets/virus.png', 32, 37);
         game.load.spritesheet('car', 'assets/racecar.png', 624.5,300);
@@ -26,17 +26,18 @@ demo.chasm.prototype = {
     create: function(){
 
         track = game.add.sprite(0,0,'track');
-        textbox = game.add.sprite(500,game.world.height - 150 ,'textbox');
+        track.scale.set(.75);
+        textbox = game.add.sprite(200,game.world.height - 150 ,'textbox');
+        textbox.scale.set(.6);
 
-
-        virus = game.add.sprite(64, game.world.height - 250, 'virus');
+        virus = game.add.sprite(64, game.world.height - 350, 'virus');
 
         virus.scale.set(3.5);
 
         anim = virus.animations.add('run');
         anim.play(speed,true);
 
-        car = game.add.sprite(64, game.world.height - 400, 'car');
+        car = game.add.sprite(64, game.world.height - 500, 'car');
         car.scale.set(.5);
 
         anim = car.animations.add('run');
@@ -52,18 +53,19 @@ demo.chasm.prototype = {
         gameovertext = game.add.text(game.world.width/2-100, game.world.height/2, '', { fontSize: '64px', fill: '#000' });
 
 
-        bmd = game.make.bitmapData(1000, 400);
-        bmd.context.font = '32px Arial';
+        bmd = game.make.bitmapData(1000, 1000);
+        bmd.context.font = '24px Arial';
         bmd.context.fillStyle = '#000';
         bmd.context.fillText('Type to start', 64, 64);
         correct = word.split("");
 
         console.log(correct);
-        bmd.addToWorld(500,750,0,0,1,1);
+        bmd.addToWorld(200,750,0,0,1,1);
         game.input.keyboard.addCallbacks(this, null, null, keyPress);
 
-        promptText = game.add.text(game.world.width/2-100, 50, word, { fontSize: '32px', fill: '#000' });
-
+        promptWord = "Type this phrase: " + word
+        promptText = game.add.text(game.world.width/2-500, 50, promptWord, { fontSize: '20px', fill: '#000', backgroundColor: "#fff" });
+        
 
 
     },
@@ -102,62 +104,55 @@ function moveVirus() {
 function moveCar() {
     car.x += 100;
 }
+var x = 64;
+var y = 64;
 function keyPress(char) {
     //  Clear the BMD
     bmd.cls();
 
-    //  Set the x value we'll start drawing the text from
-    var x = 64;
 
-    //  Loop through each letter of the word being entered and check them against the key that was pressed
 
-    // for (var i = 0; i < word.length; i++)
-    // {
-    //     var letter = word.charAt(i);
-
-    //     //  If they pressed one of the letters in the word, flag it as correct
-    //     if (char === letter)
-    //     {
-    //         correct[letter] = 'pressed';
-    //     }
-
-    //     //  Now draw the word, letter by letter, changing colour as required
-    //     if (correct[letter])
-    //     {
-    //         bmd.context.fillStyle = '#00ff00';
-    //     }
-    //     else
-    //     {
-    //         bmd.context.fillStyle = '#000000';
-    //     }
-
-    //     bmd.context.fillText(letter, x, 64);
-
-    //     x += bmd.context.measureText(letter).width;
-    // }
 
 
 
     console.log("prev", previous);
     console.log("correct", correct);
-
+    console.log(game.input.keyboard.lastKey.keyCode);
     if (game.input.keyboard.lastChar == correct[0]){
+
         removed = correct.shift();
         previous += removed;
         bmd.context.fillStyle = '#00ff00';
-        bmd.context.fillText(previous,x,64);
+        bmd.context.fillText(previous,x,y);
 
         if(removed == " "){
             moveCar();
         }
+
+        if(removed == "."){
+
+        }
+
     
 
-    }   
-    else{
+    }
+    else if(game.input.keyboard.lastKey.keyCode == 13) {
+        previous = previous.slice(0,-1);
 
+        if (correct.join() == previous){
+            bmd.context.fillStyle = '#00ff00';  
+        }
+        else{
+            bmd.context.fillStyle = '#ff0000';            
+        }
+
+        bmd.context.fillText(previous,x,y);
+    }
+    else{
+        previous += game.input.keyboard.lastChar;
         bmd.context.fillStyle = '#ff0000';
-        bmd.context.fillText(game.input.keyboard.lastChar,64,64);
-        correct.shift();
+        bmd.context.fillText(previous,64,64);
+
     }
 
 
