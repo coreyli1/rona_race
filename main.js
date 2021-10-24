3e00cd384eb10301fb13017964f396bc25905375
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2021-10-24 18:03:33 -0500

    added level2 to state manager

--- main.js
+++ main.js
@@ -7,4 +7,5 @@ game.state.add('title',demo.titlescreen);
 game.state.add('instructions',demo.instructions);
 
 game.state.add('chasm', demo.chasm);
+game.state.add('level2', demo.level2);
 game.state.start('title');
